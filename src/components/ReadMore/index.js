9991373d7f44174813a9bd45e7d61f4a61532710
cdurgaprasad <dprasad.ch10@gmail.com>
2025-02-27 07:50:52 +0000

    final commit

--- src/components/ReadMore/index.js
+++ src/components/ReadMore/index.js
@@ -1,1 +1,42 @@
 // Write your code here
+import {useState} from 'react'
+import {
+  BgContainer,
+  ReactHeading,
+  ReactDes,
+  ReactImg,
+  ReactDescription,
+  Button,
+  LargeCon,
+} from './styledComponents'
+
+const ReadMore = props => {
+  const {reactHooksDescription} = props
+  const [readButton, setReadButton] = useState(false)
+
+  const readBtnClicked = () => {
+    setReadButton(prev => !prev)
+  }
+  const btn = readButton ? 'Read Less' : 'Read More'
+  const description = readButton
+    ? reactHooksDescription
+    : reactHooksDescription.slice(0, 170)
+  return (
+    <LargeCon>
+      <BgContainer>
+        <ReactHeading>React Hooks</ReactHeading>
+        <ReactDes>Hooks are a new addition to React</ReactDes>
+        <ReactImg
+          src="https://assets.ccbp.in/frontend/hooks/react-hooks-img.png"
+          alt="react hooks"
+        />
+        <ReactDescription>{description}</ReactDescription>
+      </BgContainer>
+      <Button type="button" onClick={readBtnClicked}>
+        {btn}
+      </Button>
+    </LargeCon>
+  )
+}
+
+export default ReadMore
--- src/components/ReadMore/styledComponents.js
+++ src/components/ReadMore/styledComponents.js
@@ -1,1 +1,76 @@
 // Style your elements here
+import styled from 'styled-components'
+
+export const LargeCon = styled.div`
+  @media screen and (min-width: 768px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`
+
+export const BgContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 20px;
+  @media screen and (min-width: 768px) {
+    margin: 30px;
+  }
+`
+
+export const ReactHeading = styled.h1`
+  font-family: 'Roboto';
+  color: #334155;
+  font-size: 25px;
+  margin: 0px;
+  @media screen and (min-width: 768px) {
+    margin-top: 50px;
+  }
+`
+
+export const ReactDes = styled.p`
+  font-family: 'Roboto';
+  color: #334155;
+  font-size: 14px;
+  font-weight: 500;
+  @media screen and (min-width: 768px) {
+    font-size: 12px;
+  }
+`
+
+export const ReactImg = styled.img`
+  width: 300px;
+  @media screen and (min-width: 768px) {
+    width: 450px;
+    margin-top: 20px;
+  }
+`
+
+export const ReactDescription = styled.p`
+  font-family: 'Roboto';
+  color: #334155;
+  font-size: 14px;
+  line-height: 1.7;
+  font-weight: 500;
+  @media screen and (min-width: 768px) {
+    width: 450px;
+    font-size: 12px;
+  }
+`
+
+export const Button = styled.button`
+  font-family: 'Roboto';
+  background-color: #1f81ff;
+  outline: none;
+  height: 31px;
+  width: 100px;
+  border: none;
+  border-radius: 6px;
+  font-size: 13px;
+  color: #ffffff;
+  margin-left: 20px;
+  @media screen and (min-width: 768px) {
+    text-align: center;
+  }
+`
